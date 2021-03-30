1a5fbb68ffea01e82f8a2f0b8b0e4d69dbd02563
Chris Erman <chris.erman@khk.ee>
2021-03-30 19:51:50 +0300

    3.4.1

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -1,9 +1,8 @@
 package ee.khk;
 
 import javafx.application.Application;
-import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 
@@ -18,18 +17,18 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
 
-        HBox hbox = new HBox();
-        Button button1 = new Button("Add");
-        Button button2 = new Button("Remove");
-        HBox.setHgrow(button1, Priority.ALWAYS);
-        HBox.setHgrow(button2, Priority.ALWAYS);
-        button1.setMaxWidth(Double.MAX_VALUE);
-        button2.setMaxWidth(Double.MAX_VALUE);
-        hbox.getChildren().addAll(button1, button2);
-        Scene scene = new Scene(hbox, 500, 150);
+        Label label1 = new Label("Label1");
+        Label label2 = new Label("Label2");
+        Label label3 = new Label("Label3");
+        Label label4 = new Label("Label4");
+        Label label5 = new Label("Label5");
+        Label label6 = new Label("Label6");
+        Label label7 = new Label("Label7");
+        VBox root = new VBox(10, label1, label2, label3, label4, label5, label6, label7);
+        Scene scene = new Scene(root, 500, 150);
         stage.setScene(scene);
 
-        stage.setTitle("HBOX in JavaFX");
+        stage.setTitle("VBox in JavaFX");
 
         stage.show();
     }
