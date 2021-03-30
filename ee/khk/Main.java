8b98a4e4d4e39f28cf528714c29cfe1804e079dc
Chris Erman <chris.erman@khk.ee>
2021-03-30 13:58:04 +0300

    2.1

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -3,8 +3,8 @@ package ee.khk;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.Group;
-import javafx.scene.control.Button;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 
 public class Main extends Application {
 
@@ -14,13 +14,11 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws Exception {
 
-        Button btn = new Button();
-        btn.setText("Click!");
-        btn.setOnAction(actionEvent -> btn.setText("You've Clicked!"));
-        Group root = new Group(btn);
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
         Scene scene = new Scene(root);
+
         stage.setScene(scene);
 
         stage.setTitle("Hello JavaFX");
