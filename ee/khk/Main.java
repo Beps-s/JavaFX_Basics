43e46a9245ecdbc33e2e9a46203f78383b68a4e3
Chris Erman <chris.erman@khk.ee>
2021-03-30 20:22:00 +0300

    3.6.3

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -1,10 +1,10 @@
 package ee.khk;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -21,8 +21,12 @@ public class Main extends Application {
     public void start(Stage stage) {
         Label headlbl = new Label("Press the login button");
         StackPane.setAlignment(headlbl, Pos.TOP_CENTER);
+        StackPane.setMargin(headlbl, new Insets(10, 0, 0, 0));
+
         Label statuslbl = new Label("Start screen");
         StackPane.setAlignment(statuslbl, Pos.BOTTOM_CENTER);
+        StackPane.setMargin(statuslbl, new Insets(0, 0, 10, 0));
+
         Button loginBtn = new Button("Login");
 
         StackPane root = new StackPane(headlbl, statuslbl, loginBtn);
