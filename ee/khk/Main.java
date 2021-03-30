2bef01a6c41a60a37f664d418cdfe54ec03d05e4
Chris Erman <chris.erman@khk.ee>
2021-03-30 13:27:11 +0300

    1.3.1

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -2,6 +2,11 @@ package ee.khk;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class Main extends Application {
 
@@ -11,9 +16,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
+
+        Label label = new Label("Hello");
+        Button button = new Button("Button");
+        Group group = new Group(button);
+        FlowPane root =new FlowPane(label, group);
+        Scene scene = new Scene(root, 300, 150);
+        stage.setScene(scene);
+
         stage.setTitle("Hello JavaFX");
-        stage.setWidth(250);
-        stage.setHeight(150);
         stage.show();
     }
 }
