64869f8565dc1a8676082817934c0a9cc97a067d
Chris Erman <chris.erman@khk.ee>
2021-03-30 13:41:24 +0300

    1.5

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -3,8 +3,8 @@ package ee.khk;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
 import javafx.scene.Group;
+import javafx.scene.control.Button;
 
 public class Main extends Application {
 
@@ -16,11 +16,16 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
 
-        Group root = new Group();
-        Scene scene = new Scene(root, 400, 150, Color.BLUE);
+        Button btn = new Button();
+        btn.setText("Click!");
+        btn.setOnAction(actionEvent -> btn.setText("You've Clicked!"));
+        Group root = new Group(btn);
+        Scene scene = new Scene(root);
         stage.setScene(scene);
 
         stage.setTitle("Hello JavaFX");
+        stage.setWidth(250);
+        stage.setHeight(200);
 
         stage.show();
     }
