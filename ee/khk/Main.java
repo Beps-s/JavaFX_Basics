f54bdf4a4d60edabe7a614d9a7a3aee291be86f7
Chris Erman <chris.erman@khk.ee>
2021-03-30 20:11:24 +0300

    3.5.2

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -1,6 +1,7 @@
 package ee.khk;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
@@ -16,9 +17,17 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
+        Button left = new Button("Left");
+        BorderPane.setAlignment(left, Pos.CENTER);
+        Button right = new Button("Left");
+        BorderPane.setAlignment(right, Pos.BOTTOM_LEFT);
+        Button top = new Button("Left");
+        BorderPane.setAlignment(top, Pos.CENTER);
+        Button bottom = new Button("Left");
+        BorderPane.setAlignment(bottom, Pos.CENTER);
 
-        Button btn = new Button("Hello");
-        BorderPane root = new BorderPane(btn);
+        Button center = new Button("Center");
+        BorderPane root = new BorderPane(center, top, right, bottom, left);
 
         Scene scene = new Scene(root, 300, 150);
         stage.setScene(scene);
