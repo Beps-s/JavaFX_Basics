295142ae2f61a615f67d4e971c2189676293ebbc
Chris Erman <chris.erman@khk.ee>
2021-04-04 19:30:02 +0300

    4.12

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -1,12 +1,10 @@
 package ee.khk;
 
 import javafx.application.Application;
-import javafx.geometry.Orientation;
 import javafx.scene.control.*;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.control.MultipleSelectionModel;
 
 public class Main extends Application {
 
@@ -18,42 +16,17 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
 
-        TreeItem<String> rootTreeNode = new TreeItem<>("Languages");
+        Label lbl = new Label("Label");
+        lbl.setTooltip(new Tooltip("This is a label"));
 
-        TreeItem<String> germanics = new TreeItem<>("Germanic");
-        germanics.getChildren().add(new TreeItem<>("German"));
-        germanics.getChildren().add(new TreeItem<>("English"));
+        Button btn = new Button("Click");
+        btn.setTooltip(new Tooltip("Click the button \nto start an action"));
+        FlowPane root = new FlowPane(10, 10, btn, lbl);
 
-        TreeItem<String> romans = new TreeItem<>("Roman");
-        romans.getChildren().add(new TreeItem<>("French"));
-        romans.getChildren().add(new TreeItem<>("Spanish"));
-        romans.getChildren().add(new TreeItem<>("Italian"));
+        Scene scene = new Scene(root, 250, 250);
 
-        rootTreeNode.getChildren().add(germanics);
-        rootTreeNode.getChildren().add(romans);
-
-        TreeView<String> langsTreeView = new TreeView<>(rootTreeNode);
-        langsTreeView.setPrefSize(150, 200);
-
-        MultipleSelectionModel<TreeItem<String>> selectionModel = langsTreeView.getSelectionModel();
-
-        Label lbl = new Label();
-        selectionModel.selectedItemProperty().addListener((observableValue, oldValue, newValue) -> {
-            if (newValue != null){
-                StringBuilder path = new StringBuilder(newValue.getValue());
-                TreeItem<String> parent = newValue.getParent();
-                while (parent != null){
-                    path.insert(0, parent.getValue() + " / ");
-                    parent = parent.getParent();
-                }
-                lbl.setText(path.toString());
-            }
-        });
-
-        FlowPane root = new FlowPane(Orientation.VERTICAL, 10, 10, langsTreeView, lbl);
-        Scene scene = new Scene(root, 300, 250);
         stage.setScene(scene);
-        stage.setTitle("TreeView in JavaFX");
+        stage.setTitle("Tooltip in JavaFX");
         stage.show();
     }
 }
