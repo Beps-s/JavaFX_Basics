babb9a59873fb36b146d83f07f14d80403e856f4
Chris Erman <chris.erman@khk.ee>
2021-04-04 18:46:15 +0300

    4.8.2

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -1,12 +1,15 @@
 package ee.khk;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.*;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.MultipleSelectionModel;
 
 public class Main extends Application {
 
@@ -18,8 +21,18 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
 
+        Label selectedlbl = new Label();
         ObservableList<String> langs = FXCollections.observableArrayList("Java", "JavaScript", "C#", "Python");
         ListView<String> langsListView = new ListView<String>(langs);
+        langsListView.setPrefSize(250, 150);
+
+        final MultipleSelectionModel<String> langsSelecionModel = langsListView.getSelectionModel();
+        langsSelecionModel.selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String oldValue, String newValue) {
+                selectedlbl.setText("Selected: " + newValue);
+            }
+        });
 
         FlowPane root = new FlowPane(langsListView);
         Scene scene = new Scene(root, 250, 200);
