4536d7310a8bbe7d6225cb7e52aaa4f0da8b3d88
Chris Erman <chris.erman@khk.ee>
2021-04-04 18:11:30 +0300

    4.4.1

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -2,12 +2,13 @@ package ee.khk;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
-import javafx.scene.control.ToggleButton;
+import javafx.geometry.Orientation;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-
 
 public class Main extends Application {
 
@@ -19,29 +20,27 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
 
-        ToggleButton lightbtn = new ToggleButton("Light");
-        Label statelbl = new Label();
-        lightbtn.setOnAction(event -> {
-            if(lightbtn.isSelected()){
-                statelbl.setText("Light on");
-            }
-            else{
-                statelbl.setText("Light off...");
-            }
-        });
+        Label selectedlbl = new Label();
+        RadioButton javaBtn = new RadioButton("Java");
+        RadioButton jsBtn = new RadioButton("JavaScript");
+        RadioButton csharpBtn = new RadioButton("C#");
 
+        ToggleGroup group = new ToggleGroup();
+        javaBtn.setToggleGroup(group);
+        jsBtn.setToggleGroup(group);
+        csharpBtn.setToggleGroup(group);
 
+        javaBtn.setOnAction(event -> selectedlbl.setText("Selected Java"));
+        jsBtn.setOnAction(event -> selectedlbl.setText("Selected JavaScript"));
+        csharpBtn.setOnAction(event -> selectedlbl.setText("Selected C#"));
 
-        FlowPane root = new FlowPane(10, 10);
-        root.getChildren().addAll(lightbtn, statelbl);
+        FlowPane root = new FlowPane(Orientation.VERTICAL, 10, 10);
+        root.getChildren().addAll(javaBtn, jsBtn, csharpBtn, selectedlbl);
         root.setPadding(new Insets(10));
         Scene scene = new Scene(root, 300, 150);
+
         stage.setScene(scene);
-
-        stage.setTitle("Flowpane in JavaFX");
-        stage.setWidth(300);
-        stage.setHeight(200);
-
+        stage.setTitle("RadioButtons in JavaFX");
         stage.show();
     }
 }
