98890f5643b56534add15119a9f51683240e9a08
Chris Erman <chris.erman@khk.ee>
2021-04-04 18:18:53 +0300

    4.4.3

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -1,17 +1,12 @@
 package ee.khk;
 
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.Toggle;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 
 public class Main extends Application {
 
@@ -21,31 +16,30 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
 
         Label selectedlbl = new Label();
+        Button selectedBtn = new Button("Select");
 
         RadioButton javaBtn = new RadioButton("Java");
         RadioButton jsBtn = new RadioButton("JavaScript");
         RadioButton csharpBtn = new RadioButton("C#");
 
         ToggleGroup group = new ToggleGroup();
+
         javaBtn.setToggleGroup(group);
         jsBtn.setToggleGroup(group);
         csharpBtn.setToggleGroup(group);
 
-        group.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
-            @Override
-            public void changed(ObservableValue<? extends Toggle> changed, Toggle oldValue, Toggle newValue) {
-                RadioButton selectedBtn = (RadioButton) newValue;
-                selectedBtn.setText("Selected: " + selectedBtn.getText());
-            }
+        selectedBtn.setOnAction(event -> {
+            RadioButton selected = (RadioButton) group.getSelectedToggle();
+            selectedlbl.setText("Selected: " + selected.getText());
         });
 
         FlowPane root = new FlowPane(Orientation.VERTICAL, 10, 10);
-        root.getChildren().addAll(javaBtn, jsBtn, csharpBtn, selectedlbl);
+        root.getChildren().addAll(javaBtn, jsBtn, csharpBtn, selectedBtn, selectedlbl);
         root.setPadding(new Insets(10));
-        Scene scene = new Scene(root, 300, 150);
+        Scene scene = new Scene(root, 250, 200);
 
         stage.setScene(scene);
         stage.setTitle("RadioButtons in JavaFX");
