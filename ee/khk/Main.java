33f2813048867b7fa6471c659d860fdee2e18716
Chris Erman <chris.erman@khk.ee>
2021-04-04 18:49:56 +0300

    4.8.3

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -5,6 +5,7 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Orientation;
 import javafx.scene.control.*;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
@@ -22,19 +23,26 @@ public class Main extends Application {
     public void start(Stage stage) {
 
         Label selectedlbl = new Label();
+
         ObservableList<String> langs = FXCollections.observableArrayList("Java", "JavaScript", "C#", "Python");
         ListView<String> langsListView = new ListView<String>(langs);
         langsListView.setPrefSize(250, 150);
 
         final MultipleSelectionModel<String> langsSelecionModel = langsListView.getSelectionModel();
+        langsSelecionModel.setSelectionMode(SelectionMode.MULTIPLE);
         langsSelecionModel.selectedItemProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observableValue, String oldValue, String newValue) {
-                selectedlbl.setText("Selected: " + newValue);
+                String selectedItems ="";
+                ObservableList<String> selected = langsSelecionModel.getSelectedItems();
+                for (String item : selected){
+                    selectedItems += item + " ";
+                }
+                selectedlbl.setText("Selected: " + selectedItems);
             }
         });
 
-        FlowPane root = new FlowPane(langsListView);
+        FlowPane root = new FlowPane(Orientation.VERTICAL, 10, 10, selectedlbl, langsListView);
         Scene scene = new Scene(root, 250, 200);
 
         stage.setScene(scene);
