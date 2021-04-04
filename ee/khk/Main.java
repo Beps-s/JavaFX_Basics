50954d6a966a5a01661ce43b51cfdcd3cd726f2f
Chris Erman <chris.erman@khk.ee>
2021-04-04 18:26:25 +0300

    4.6.1

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -1,9 +1,7 @@
 package ee.khk;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
-import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
@@ -19,18 +17,20 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
 
-        Label lbl = new Label();
-        TextArea textArea = new TextArea();
-        textArea.setPrefColumnCount(15);
-        textArea.setPrefRowCount(5);
-        Button btn = new Button("Click");
-        btn.setOnAction(event -> lbl.setText("Input: " + textArea.getText()));
-        FlowPane root = new FlowPane(Orientation.VERTICAL, 10, 10, textArea, btn, lbl);
-        root.setAlignment(Pos.CENTER);
+        Label headerLbl = new Label("ScrollPane");
+        Label textLbl = new Label("jou \n" +
+                "jou jou");
+
+        ScrollPane scrollPane = new ScrollPane(textLbl);
+        scrollPane.setPrefViewportHeight(150);
+        scrollPane.setPrefViewportWidth(200);
+
+
+        FlowPane root = new FlowPane(Orientation.VERTICAL, 10, 10, headerLbl, scrollPane);
         Scene scene = new Scene(root, 250, 200);
 
         stage.setScene(scene);
-        stage.setTitle("TextArea in JavaFX");
+        stage.setTitle("ScrollPane in JavaFX");
         stage.show();
     }
 }
